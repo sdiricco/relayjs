c5f75565b15b2a448b6676f09e5f303ea1bfd7ef
simone <sdiricco.91@gmail.com>
2021-11-28 18:44:42 +0100

    up

--- README.md
+++ README.md
@@ -13,38 +13,34 @@ If you have problem see [below]()
 ## Basic Usage
 
 ```javascript
- const{RelayJs} = require('@sdiricco/relayjs');
- 
- let main = async () => {
- 
-   const relay = new RelayJs();
- 
-   try {
- 
-     relay.on("error", (e) => {
-       console.log("error raised:", e)
-     });
- 
-     console.log("connecting..");
-     await relay.connect();
-     console.log("connected");
+const { RelayJs } = require("@sdiricco/relayjs");
 
-     await relay.write(13, relay.CLOSE);
+let main = async () => {
+  const relay = new RelayJs();
 
-     console.log(relay.relays);
-     
-     if (relay.relays[13].state === relay.CLOSE) {
-        console.log('relay 13 close')
-     } else{
-        console.log('relay 13 open')
-     }
-     
-   } catch (e) {
-     console.log("error catched:", e);
-   }
+  try {
+    relay.on("error", (e) => {
+      console.log("error raised:", e);
+    });
 
- }
- 
- main();
+    console.log("connecting..");
+    await relay.connect();
+    console.log("connected");
+
+    await relay.write(13, relay.CLOSE);
+
+    console.log(relay.relays);
+
+    if (relay.relays[13].state === relay.CLOSE) {
+      console.log("relay 13 close");
+    } else {
+      console.log("relay 13 open");
+    }
+  } catch (e) {
+    console.log("error catched:", e);
+  }
+};
+
+main();
 ```
 
--- test/test-connect-001.js
+++ test/test-connect-001.js
@@ -1,80 +1,76 @@
 /**
- * 
+ *
  * Scope of test:
  * Verify the functionallity of connect() method
  * in auto-connect mode
- * 
+ *
  * Prerequisites:
  * - a board with a valid firmata.ino firmware connected
- * 
+ *
  * Description step:
  * - call <constructor()> of RelayJs class
  * - listen on <error> event
  * - call <connect()> method with no parameters. auto-connect mode.
- * 
- * Asserts: 
+ *
+ * Asserts:
  * - connected property
  * - result of connect() method
- * 
+ *
  */
 
- const{RelayJs} = require('../index');
- const { Test, prompt } = require("./utils");
- 
- let main = async () => {
- 
-   const test = new Test(
-     module.filename,
-     "Verify the functionallity of connect() method in auto-connect mode"
-   );
- 
-   const relay = new RelayJs({
-     inverseOut: true
-   });
-   let __connect = false;
- 
-   try {
- 
-     relay.on("error", (e) => {
-       console.log("error raised:", e)
-     });
+const { RelayJs } = require("../index");
+const { Test, prompt } = require("./utils");
 
+let main = async () => {
+  const test = new Test(
+    module.filename,
+    "Verify the functionallity of connect() method in auto-connect mode"
+  );
 
- 
-     console.log("connecting..");
-     __connect = await relay.connect();
-     console.log("connected");
+  const relay = new RelayJs({
+    inverseOut: true,
+  });
+  let __connect = false;
 
-     await relay.reset();
+  try {
+    relay.on("error", (e) => {
+      console.log("error raised:", e);
+    });
 
-     console.log(relay.__relaysT)
-     console.log(relay.relays.length)
-     console.log(relay.board.pins)
+    console.log("connecting..");
+    __connect = await relay.connect();
+    console.log("connected");
+
+    await relay.reset();
+
+    console.log(relay.__relaysT);
+    console.log(relay.relays.length);
+    console.log(relay.board.pins);
 
     //  await prompt.get('disconnect the board')
 
-     await relay.write(0, relay.CLOSE);
+    await relay.write(0, relay.CLOSE);
 
-     console.log(relay.relays);
-     
-     if (relay.relays[0].state === relay.CLOSE) {
-        console.log('RELAY CLOSE')
-     }
-     if (relay.relays[0].state === relay.OPEN) {
-      console.log('RELAY OPEN')
-     }
+    console.log(relay.relays);
 
-     console.log(relay.read(0));
-     console.log(relay.read(5));
-     
-   } catch (e) {
-     console.log("error catched:", e);
-   }
- 
-   test.assert(relay.connected, "connected property");
-   test.assert(__connect, "result of connect() method");
-   test.end({exit: true});
- 
- }
- 
- main();
+    if (relay.relays[0].state === relay.CLOSE) {
+      console.log("RELAY CLOSE");
+    }
+    if (relay.relays[0].state === relay.OPEN) {
+      console.log("RELAY OPEN");
+    }
+
+    console.log(relay.read(0));
+    console.log(relay.read(5));
+  } catch (e) {
+    console.log("error catched:", e);
+  }
+
+  test.assert(relay.connected, "connected property");
+  test.assert(__connect, "result of connect() method");
+  test.end({ exit: true });
+};
+
+main();
+
+
